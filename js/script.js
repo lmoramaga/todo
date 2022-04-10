b0335add4dedcc6fa698d7f01fb9164cf09939e5
lmoramaga <98915967+lmoramaga@users.noreply.github.com>
2022-04-10 22:45:26 +0200

    declarations

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const taskInput = document.querySelector(".task-input input");
+const dateInput = document.getElementById("dueDate");
+const filters = document.querySelectorAll(".filters span");
+const clearAll = document.querySelector(".clear-btn");
+const addBtn = document.getElementById("add-btn");
+const taskBox = document.querySelector(".task-box");
