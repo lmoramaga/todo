38e03d555b6836063bae3f57e419d4dba350016f
lmoramaga <98915967+lmoramaga@users.noreply.github.com>
2022-04-10 22:47:21 +0200

    task listing

--- js/script.js
+++ js/script.js
@@ -4,3 +4,50 @@ const filters = document.querySelectorAll(".filters span");
 const clearAll = document.querySelector(".clear-btn");
 const addBtn = document.getElementById("add-btn");
 const taskBox = document.querySelector(".task-box");
+
+let editId,
+  isEditTask = false,
+  todos = JSON.parse(localStorage.getItem("todo-list"));
+
+filters.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    document.querySelector("span.active").classList.remove("active");
+    btn.classList.add("active");
+    showTodo(btn.id);
+  });
+});
+
+function showTodo(filter) {
+    let liTag = "";
+    if (todos) {
+      todos.forEach((todo, id) => {
+        const completed = todo.status === "completed" ? "checked" : "";
+        if (filter === todo.status || filter === "all") {
+          liTag += `<li class="task">
+                              <label for="${id}">
+                                  <input onclick="updateStatus(this)" type="checkbox" id="${id}" ${completed}>
+                                  <div class="tasks"><p class="${completed}">${todo.name}<p class="dueDate">Due: ${todo.dueDate}</p></p></div>
+                              </label>
+                              <div class="settings">
+                                  <i onclick="showMenu(this)" class="uil uil-ellipsis-h"></i>
+                                  <ul class="task-menu">
+                                      <li onclick='editTask(${id}, "${todo.name}")'>
+                                      <i class="uil uil-pen"></i>Edit</li>
+                                      <li onclick='deleteTask(${id}, "${filter}")'><i class="uil uil-trash"></i>Delete</li>
+                                  </ul>
+                              </div>
+                          </li>`;
+        }
+      });
+    }
+    taskBox.innerHTML = liTag || `<span>You don't have any task here</span>`;
+    const checkTask = taskBox.querySelectorAll(".task");
+    !checkTask.length
+      ? clearAll.classList.remove("active")
+      : clearAll.classList.add("active");
+    taskBox.offsetHeight >= 300
+      ? taskBox.classList.add("overflow")
+      : taskBox.classList.remove("overflow");
+  }
+  showTodo("all");
+  
